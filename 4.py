450e2c47a995fcc8edd697ef3a7c273947158b87
Александр Черных <55315983+TermosW@users.noreply.github.com>
2019-09-16 20:01:03 +0500

    Update 4.py

--- 4.py
+++ 4.py
@@ -2,8 +2,8 @@ num = 0
 
 for firstNumber in range(100, 999, 1):
 	for secondNumber in range(100, 999, 1):
-		result = firstNumber*secondNumber
+		result = firstNumber * secondNumber
 		if result > num and str(result) == str(result)[::-1]:
-        	num = result
+			num = result
 print(num)
 
